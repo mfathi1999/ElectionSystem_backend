e01a6707ea99a6989d9f2ecfeb8e143cb3f9eb15
Mohammad Fathi <fathimohammad1999@gmail.com>
2022-09-04 18:43:03 +0430

    File and Document models created and their relations implemented

---
+++ database/migrations/2022_09_04_134825_create_documents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status',['CHECK','ACCEPT','REJECT'])->default('CHECK');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
